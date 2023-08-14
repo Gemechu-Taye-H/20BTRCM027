a2134094935fd58e57b91b90a0bd0f698d2b80fb
Gemechu <20btrcm027@jainuniversity.ac.in>
2023-08-14 20:27:51 +0530

    moved componentfolder to src folder and updated import in app.js

--- Train-Schedule/src/App.js
+++ Train-Schedule/src/App.js
@@ -1,42 +1,12 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import TrainList from './components/TrainList'; // Import the TrainList component
 
-const TrainList = ({ authToken }) => {
-  const [setTrains] = useState([]);
-  const [setSpecificTrain] = useState(null);
+function App() {
+  return (
+    <div className="App">
+      <TrainList />
+    </div>
+  );
+}
 
-  useEffect(() => {
-    const fetchTrains = async () => {
-      try {
-        const response = await axios.get('http://20.244.56.144/train/trains', {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        });
-        setTrains(response.data);
-      } catch (error) {
-        console.error('Error fetching trains:', error);
-      }
-    };
-
-    const fetchSpecificTrain = async () => {
-      try {
-        const response = await axios.get('http://20.244.56.144/train/trains/2344', {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        });
-        setSpecificTrain(response.data);
-      } catch (error) {
-        console.error('Error fetching specific train:', error);
-      }
-    };
-
-    fetchTrains();
-    fetchSpecificTrain();
-  }, [authToken, setSpecificTrain, setTrains]);
-
-  // ... The rest of your TrainList component code
-};
-
-export default TrainList;
+export default App;
